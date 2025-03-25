27c61db4e96f73b4339bfc4b8805d3707803ee10
Jeancarlos <jeancarlos@gmail.com>
2025-03-24 23:20:37 -0600

    carga de login

---
+++ Front-SACH/src/app/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'] // Ensure this file exists or create it
+})
+export class LoginComponent {
+
+}
